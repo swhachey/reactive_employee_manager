0283bfdb2d1ae2c533eb49bd6b9464a17c93b45a
swhachey <sw.hachey@gmail.com>
2021-03-23 20:21:15 -0600

    created table rows and columns

--- empire/src/components/Table/table.js
+++ empire/src/components/Table/table.js
@@ -10,7 +10,9 @@ function Table(props) {
       <th scope="col">#</th>
       <th scope="col">First</th>
       <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Email</th>
+      <th scope="col">City</th>
+      <th scope="col">Phone</th>
     </tr>
   </thead>
   <tbody>
--- empire/src/components/TableRow/tablerow.js
+++ empire/src/components/TableRow/tablerow.js
@@ -7,6 +7,8 @@ function Tablerow(props) {
       <td>{props.first}</td>
       <td>{props.last}</td>
       <td>{props.email}</td>
+      <td>{props.city}</td>
+      <td>{props.phone}</td>
     </tr>
     )
 }
