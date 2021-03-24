d023ef0d2d542da58bde9adf89d55046c66b309a
swhachey <sw.hachey@gmail.com>
2021-03-23 18:03:35 -0600

    created header jumbotron component

---
+++ empire/src/components/Header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css"
+import Table from "../Table/table"
+
+function Header() {
+  return (
+    <div className="jumbotron">
+      <h1>EMPIRE</h1>
+      <h3>
+        Organize your empire and take over the world.
+      </h3>
+    </div>
+  );
+}
+
+export default Header;
